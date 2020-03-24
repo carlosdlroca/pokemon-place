ca7378b71d11c9441998b4314ef30a02dd487ad5
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-23 20:39:52 -0700

    Add more Routes

--- src/App/Router.js
+++ src/App/Router.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
 
-const Page = ({ h }) => <h1>{h}</h1>;
+import { PokemonPage } from "../Pages/Styles";
 
 export default () => (
     <Router>
         <Switch>
             <Route exact path='/'>
-                <Page h='First' />
+                <Redirect to='/pokemon' />
             </Route>
-            <Route path='/poop'>
-                <Page h='Popo' />
+            <Route path='/pokemon/:pokemon_id'>
+                <h1> THis is for a specific pokemon</h1>
+            </Route>
+            <Route path='/pokemon'>
+                <PokemonPage />
             </Route>
         </Switch>
     </Router>
