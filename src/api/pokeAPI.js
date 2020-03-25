035aeb08f38f160e30c4d876f9be97b3f5c54e50
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-24 22:38:38 -0700

    Remove getPokemonByGeneration function

--- src/api/pokeAPI.js
+++ src/api/pokeAPI.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
 const SPRITE_URL =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-const gen = {
+const generations = {
     1: { offset: 0, limit: 151 },
     2: { offset: 151, limit: 100 },
     3: { offset: 251, limit: 135 },
@@ -12,16 +12,12 @@ const gen = {
     7: { offset: 721, limit: 86 }
 };
 
-export async function getPokemon(offset = 0, limit = 121) {
+export async function getPokemon(genNum = 1) {
+    const { offset, limit } = generations[genNum];
     const res = await fetch(`${BASE_URL}?offset=${offset}&limit=${limit}`);
     const data = await res.json();
     return data.results.map((pokemon, idx) => {
         var id = idx + offset + 1;
         return { ...pokemon, id, sprite: `${SPRITE_URL}${id}.png` };
     });
-}
-
-export function getPokemonByGeneration(id = 1) {
-    const { offset, limit } = gen[id];
-    const arr = getPokemon(offset, limit);
 }
