aad2433f6462f1ec206022ab6762f5270a8a0c63
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-24 22:46:27 -0700

    Add temporary styling

--- src/components/List/Styles.js
+++ src/components/List/Styles.js
@@ -2,12 +2,29 @@ import styled from "styled-components";
 
 export const List = styled.ul`
     display: grid;
-    grid-template-columns: repeat(1fr, 3);
-    grid-auto-rows: 5rem;
+    grid-template-columns: repeat(1fr, 4);
+    /* grid-auto-rows: 5rem; */
     grid-gap: 2rem;
     list-style: none;
     width: 60%;
     margin: 1rem auto;
 `;
 
-export const ListItem = styled.li``;
+export const ListItem = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    border: 1px solid black;
+
+    img {
+        --img-size: 100px;
+        display: block;
+        height: var(--img-size);
+        width: var(--img-size);
+    }
+
+    &:nth-of-type(4n) {
+        grid-column: 4 / span 1;
+    }
+`;
