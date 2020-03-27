45b5e7ae9771e095c8718bfbb984e3907973ccf4
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-27 10:29:41 -0700

    Wrap fetch in try-catch block

--- src/api/pokeAPI.js
+++ src/api/pokeAPI.js
@@ -14,27 +14,35 @@ const generations = {
 
 export async function getPokemonByGeneration(genNum = 1) {
     const { offset, limit } = generations[genNum];
-    const res = await fetch(`${BASE_URL}?offset=${offset}&limit=${limit}`);
-    const data = await res.json();
-    return data.results.map((pokemon, idx) => {
-        var id = idx + offset + 1;
-        return { ...pokemon, id, sprite: `${SPRITE_URL}${id}.png` };
-    });
+    try {
+        const res = await fetch(`${BASE_URL}?offset=${offset}&limit=${limit}`);
+        const data = await res.json();
+        return data.results.map((pokemon, idx) => {
+            var id = idx + offset + 1;
+            return { ...pokemon, id, sprite: `${SPRITE_URL}${id}.png` };
+        });
+    } catch (err) {
+        console.error({ err });
+    }
 }
 
 export async function getPokemon(id) {
-    const res = await fetch(`${BASE_URL}/${id}`);
-    const data = await res.json();
-    return {
-        id: data.id,
-        name: data.name,
-        abilities: data.abilities,
-        location_area_encounters: data.location_area_encounters,
-        species: data.species,
-        sprites: { ...data.sprites },
-        stats: data.stats,
-        types: data.type,
-        weight: data.weight,
-        moves: data.moves
-    };
+    try {
+        const res = await fetch(`${BASE_URL}/${id}`);
+        const data = await res.json();
+        return {
+            id: data.id,
+            name: data.name,
+            abilities: data.abilities,
+            location_area_encounters: data.location_area_encounters,
+            species: data.species,
+            sprites: { ...data.sprites },
+            stats: data.stats,
+            types: data.type,
+            weight: data.weight,
+            moves: data.moves
+        };
+    } catch (err) {
+        console.error({ err });
+    }
 }
