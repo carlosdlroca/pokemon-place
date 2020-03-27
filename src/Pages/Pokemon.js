744b80ec5a2861072d07be0e0232e0cc9199809f
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-27 11:41:33 -0700

    Add Ability to switch generation

--- src/Pages/Pokemon.js
+++ src/Pages/Pokemon.js
@@ -6,14 +6,15 @@ import { Link } from "react-router-dom";
 
 export default () => {
     const [pokemonArr, setPokemonArr] = useState([]);
+    const [generation, setGeneration] = useState(1);
 
     useEffect(() => {
         const setPokemonFromApi = async () => {
-            const pokemon = await getPokemonByGeneration(1);
+            const pokemon = await getPokemonByGeneration(generation);
             setPokemonArr(pokemon);
         };
         setPokemonFromApi();
-    }, []);
+    }, [generation]);
 
     const renderPokemon = (pokemon, itemIndex) => (
         <React.Fragment>
@@ -22,12 +23,27 @@ export default () => {
         </React.Fragment>
     );
 
+    const onChange = e => {
+        const { value } = e.target;
+        setGeneration(Number(value));
+    };
+
     if (!pokemonArr.length) {
         return <h1>Loading...</h1>;
     }
 
     return (
         <PokemonPage>
+            <h3>Select a generation: </h3>
+            <select onChange={onChange}>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+            </select>
             <List renderListItemChildren={renderPokemon} items={pokemonArr} />
         </PokemonPage>
     );
