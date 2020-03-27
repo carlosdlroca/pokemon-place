e1c504219b154542595f713a7bfe62cd4b128276
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-26 23:06:40 -0700

    Create Pokemon Page

---
+++ src/Pages/SinglePokemon.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { PokemonPage } from "./Styles";
+import { useParams } from "react-router-dom";
+import { getPokemon } from "../api/pokeAPI";
+
+export default () => {
+    const { pokemon_id } = useParams();
+
+    const [pokemon, setPokemon] = useState(null);
+
+    useEffect(() => {
+        const pokemonApi = async id => {
+            const pokemonObject = await getPokemon(id);
+            setPokemon(pokemonObject);
+        };
+        pokemonApi(pokemon_id);
+    }, [pokemon_id]);
+
+    if (!pokemon) {
+        return <h1>Loading...</h1>;
+    }
+
+    return (
+        <PokemonPage>
+            {pokemon.name}
+            <p>weight: {pokemon.weight}</p>
+            <img
+                src={pokemon.sprites["front_default"]}
+                alt='Back of the pokemon'
+            />
+            <img src={pokemon.sprites["back_default"]} alt='' />
+        </PokemonPage>
+    );
+};
