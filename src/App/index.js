70c741dd300551729cad55ec8994267af218b020
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-03-28 10:49:54 -0700

    Provide Themes to Components

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Router from "./Router";
+import { ThemeProvider } from "styled-components";
 import BaseStyle from "./Styles/BaseStyle";
 import Normalize from "./Styles/Normalize";
 
+import themes from "./Styles/themes";
+
 export default () => (
-    <React.Fragment>
+    <ThemeProvider theme={themes}>
         <BaseStyle />
         <Normalize />
         <Router />
-    </React.Fragment>
+    </ThemeProvider>
 );
