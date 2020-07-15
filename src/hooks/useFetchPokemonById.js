aa70d00d87e27a33b9dec1d27c45f4192c414920
Carlos De La Roca <carlosdlroca80@gmail.com>
2020-07-15 13:59:05 -0700

    Fix eqeqeq warnings

--- src/hooks/useFetchPokemonById.js
+++ src/hooks/useFetchPokemonById.js
@@ -4,15 +4,15 @@ import { StoreContext } from "../store";
 import { SET_SELECTED_POKEMON } from "../store/actionTypes";
 import { getPokemonById } from "../api/pokeAPI";
 
-export default id => {
+export default (id) => {
     const { pokemon_id } = useParams();
     const [state, dispatch] = useContext(StoreContext);
     useEffect(() => {
-        const pokemonApi = async id => {
+        const pokemonApi = async (id) => {
             const selectedPokemon = await getPokemonById(id);
             dispatch({ type: SET_SELECTED_POKEMON, selectedPokemon });
         };
-        if (!state.selectedPokemon || state.selectedPokemon.id != pokemon_id) {
+        if (!state.selectedPokemon || state.selectedPokemon.id !== pokemon_id) {
             pokemonApi(pokemon_id);
         }
     }, [dispatch, pokemon_id, state.selectedPokemon]);
